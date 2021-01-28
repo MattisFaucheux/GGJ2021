dc0953183c84b5d47234f64f31cf6b83713b2201
Mattis <mattis.faucheux@gmail.com>
2021-01-28 03:28:37 -0500

    Add Radar v1 for the player

---
+++ Assets/Scripts/Radar/RadarPing.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class RadarPing : MonoBehaviour
+{
+
+    private SpriteRenderer spriteRenderer;
+    private float disappearTimer = 0;
+    private float disappearTimerMax = 1;
+    private Color color = new Color(1, 1, 1, 1f);
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        disappearTimer += Time.deltaTime;
+
+        color.a = Mathf.Lerp(disappearTimerMax, 0, disappearTimer / disappearTimerMax);
+        spriteRenderer.color = color;
+
+        if (disappearTimer >= disappearTimerMax)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetColor(Color color)
+    {
+        this.color = color;
+    }
+
+    public void SetDisappearTimer(float disappearTimerMax)
+    {
+        this.disappearTimerMax = disappearTimerMax;
+        disappearTimer = 0;
+    }
+}
---
+++ Assets/Scripts/Radar/RadarPulse.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadarPulse : MonoBehaviour
+{
+    [SerializeField] 
+    private Transform pfRadarPing;
+
+    private Transform pulseTransform;
+    private float range;
+
+    public float rangeMax = 300;
+    public float rangeSpeed = 20;
+
+    public LayerMask layerToDetect;
+
+    private List<Collider> alreadyPingedColliderList;
+
+    private void Awake()
+    {
+        pulseTransform = transform.Find("Pulse");
+        alreadyPingedColliderList = new List<Collider>();
+    }
+
+    private void Update()
+    {
+        range += rangeSpeed * Time.deltaTime;
+        if (range > rangeMax)
+        {
+            range = 0;
+            alreadyPingedColliderList.Clear();
+        }
+        pulseTransform.localScale = new Vector3(range/2, range/2);
+
+        RaycastHit[] hits = Physics.SphereCastAll(transform.position - (transform.up * range), range, transform.up, 10 * range, layerToDetect);
+        if (hits.Length > 0)
+        {
+            foreach (var hit in hits)
+            {
+                if (!alreadyPingedColliderList.Contains(hit.collider))
+                {
+                    alreadyPingedColliderList.Add(hit.collider);
+                    Transform radarPingTransform = Instantiate(pfRadarPing, new Vector3(hit.collider.transform.position.x, hit.collider.transform.position.y, -1), Quaternion.identity);
+                    RadarPing radarPing = radarPingTransform.GetComponent<RadarPing>();
+                    if (radarPing)
+                    {
+                        radarPing.SetColor(Color.magenta);
+                    }
+                }
+            }
+        }
+    }
+
+}
