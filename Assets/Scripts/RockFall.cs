d8cf7fde25eb28574d5e3e94ff99dfb3af8b571d
Andoni <a.iturioz@student.isartdigital.com>
2021-01-29 17:46:33 -0500

    RockFall_Degeux

---
+++ Assets/Scripts/RockFall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockFall : MonoBehaviour
+{
+    public GameObject rock;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            rock.SetActive(true);
+            //ScreenShake
+        }
+    }
+}
