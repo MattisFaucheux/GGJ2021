e0831808991695e40951803db2254fe1a1e4f6ef
Denat <v.denat@student.isartdigital.com>
2021-01-31 16:01:48 -0500

    Dialogue Ready

--- Assets/Scripts/dialogue/DialogueTrigger.cs
+++ Assets/Scripts/dialogue/DialogueTrigger.cs
@@ -6,6 +6,15 @@ public class DialogueTrigger : MonoBehaviour
 {
     public dialogue dialogue;
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            TriggerDialogue();
+            Destroy(gameObject);
+        }
+    }
+
     public void TriggerDialogue()
     {
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
