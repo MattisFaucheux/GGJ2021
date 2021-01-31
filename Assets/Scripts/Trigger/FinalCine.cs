9fae7c9db9caa3b7fc20e8529fe775a7ce73421b
Andoni <a.iturioz@student.isartdigital.com>
2021-01-31 14:54:18 -0500

    CineFinal

---
+++ Assets/Scripts/Trigger/FinalCine.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+public class FinalCine : MonoBehaviour
+{
+    public CinemachineBrain brain;
+    public CinemachineVirtualCamera cam;
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("PodPlayer"))
+        {
+            cam.Priority = 100;
+            brain.m_DefaultBlend.m_Time = 10;
+        }
+    }
+}
