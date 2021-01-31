6fa41d117e8977e0fc6eebbb0bc6a1fceb018471
Mattis <mattis.faucheux@gmail.com>
2021-01-31 13:32:59 -0500

    add trigger DesacMaxPodDistance

---
+++ Assets/Scripts/Trigger/TriggerDesactivePodDistance.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDesactivePodDistance : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        PodController pod = other.GetComponent<PodController>();
+        if (pod)
+        {
+            pod.SetIsMaxPodDistance(false);
+        }
+    }
+}
